70c121cc5df7f73a196833e938f57e7aa5172582
Aaria Carter-Weir <aaria@mxstore.com.au>
2019-10-05 10:19:56 +1000

    updated to ignore empty event paths

--- TapAway.js
+++ TapAway.js
@@ -7,7 +7,17 @@ const bindings = [];
 
 hammer.on('tap press', e => {
     bindings.forEach(({ el, callback }) => {
-        if (e.target !== el && ! e.srcEvent.propagationPath().includes(el)) {
+        // get composed path using propagationPath polyfill which will use native browser
+        // methods if available and otherwise fallback to a custom implementation
+        const path = e.srcEvent.propagationPath();
+
+        // if there is no composed path, disregard event, it may be an anomaly
+        if (! path || ! path.length) {
+            return;
+        }
+
+        // check if the path includes the directive targeted element
+        if (e.target !== el && ! path.includes(el)) {
             callback();
         }
     });
